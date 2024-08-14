6986d23d4ddc46f7ffd0869cfd3d22622bda2156
aesperer <piyrw9754@gmail.com>
2024-08-14 14:10:57 +0900

    update :: user api

--- order/order-service/build.gradle.kts
+++ order/order-service/build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
