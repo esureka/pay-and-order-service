1a25c9fabd07bc94ea9c60d8102ec9b31e70da17
aesperer <piyrw9754@gmail.com>
2024-08-19 13:55:47 +0900

    update :: gradle java version toll

--- pay/pay-service/build.gradle.kts
+++ pay/pay-service/build.gradle.kts
@@ -9,9 +9,7 @@ group = "payorder"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+	sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
