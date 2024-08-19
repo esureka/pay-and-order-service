faa1fdab3c24ff4fa67e7ca9be81bed40496f6c6
aesperer <piyrw9754@gmail.com>
2024-08-19 09:18:48 +0900

    add :: kafka consumer setting in payservice

--- pay/pay-service/src/main/kotlin/payorder/payservice/common/config/KafkaConfig.kt
+++ pay/pay-service/src/main/kotlin/payorder/payservice/common/config/KafkaConfig.kt
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import payorder.payservice.application.product.event.OrderProductEvent
+import reactor.kafka.receiver.ReceiverOptions
 import reactor.kafka.sender.SenderOptions
 
 @Configuration
@@ -13,6 +15,19 @@ class KafkaConfig(
     @Value("\${spring.kafka.producer.bootstrap-servers}")
     val bootStrapServer: String = "localhost:9092"
 ) {
+
+    @Bean
+    fun reactiveKafkaConsumerTemplate(props: KafkaProperties): ReactiveKafkaConsumerTemplate<String, OrderProductEvent> {
+        val consumerProperties = props.buildConsumerProperties().toMutableMap().apply {
+            putAll(
+                mapOf(
+                    // value 값을 deserialize 할때 타입을 참조할 dto 을 정의
+                    "spring.json.value.default.type" to OrderProductEvent::class.java
+                )
+            )
+        }
+        return ReactiveKafkaConsumerTemplate(ReceiverOptions.create(consumerProperties))
+    }
 
     @Bean
     fun orderProductKafkaTemplate(props: KafkaProperties): ReactiveKafkaProducerTemplate<String, OrderProductEvent> =
